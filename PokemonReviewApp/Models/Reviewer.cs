0f4b195815cacea3f8e8c59540028030894b864d
Trexxin <Charlesharris2322@gmail.com>
2022-06-24 23:39:06 -0500

    Added Review & Reviewer Models

---
+++ PokemonReviewApp/Models/Reviewer.cs
@@ -0,0 +1,9 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Reviewer
+    {
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
