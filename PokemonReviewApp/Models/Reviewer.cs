dbee779c71e22b4a32fb043a48fd8c54424baa3b
Trexxin <Charlesharris2322@gmail.com>
2022-06-25 01:04:40 -0500

    Added one-to-many relationships

--- PokemonReviewApp/Models/Reviewer.cs
+++ PokemonReviewApp/Models/Reviewer.cs
@@ -5,5 +5,6 @@
         public int id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public ICollection<Review> Reviews { get; set; }
     }
 }
