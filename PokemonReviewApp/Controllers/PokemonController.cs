ea0c489d9f34fafe563ffffb48bddda4e6a180bc
Trexxin <Charlesharris2322@gmail.com>
2022-06-28 04:24:56 -0500

    Added Controller

---
+++ PokemonReviewApp/Controllers/PokemonController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController : Controller
+    {
+        private readonly IPokemonRepository _pokemonRepoitory;
+
+        public PokemonController(IPokemonRepository pokemonRepoitory)
+        {
+            _pokemonRepoitory = pokemonRepoitory;
+        }
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
+        public IActionResult GetPokemons()
+        {
+            var pokemons = _pokemonRepoitory.GetPokemons();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(pokemons);
+        }
+    }
+}
