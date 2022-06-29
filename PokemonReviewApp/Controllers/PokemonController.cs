70e0259e3efc5ce0be0266089ef095468f333676
Trexxin <Charlesharris2322@gmail.com>
2022-06-29 05:00:16 -0500

    Added Validation

--- PokemonReviewApp/Controllers/PokemonController.cs
+++ PokemonReviewApp/Controllers/PokemonController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Dto;
 using PokemonReviewApp.Interfaces;
 using PokemonReviewApp.Models;
 
@@ -9,21 +11,56 @@ namespace PokemonReviewApp.Controllers
     public class PokemonController : Controller
     {
         private readonly IPokemonRepository _pokemonRepoitory;
+        private readonly IMapper _mapper;
 
-        public PokemonController(IPokemonRepository pokemonRepoitory)
+        public PokemonController(IPokemonRepository pokemonRepoitory, IMapper mapper)
         {
             _pokemonRepoitory = pokemonRepoitory;
+            _mapper = mapper;
         }
         [HttpGet]
         [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
         public IActionResult GetPokemons()
         {
-            var pokemons = _pokemonRepoitory.GetPokemons();
+            var pokemons = _mapper.Map<List<PokemonDto>>(_pokemonRepoitory.GetPokemons());
 
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
 
             return Ok(pokemons);
+        }
+
+        [HttpGet("{pokeId}")]
+        [ProducesResponseType(200, Type = typeof(Pokemon))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemon(int pokeId)
+        {
+            if (!_pokemonRepoitory.PokemonExists(pokeId))
+                return NotFound();
+
+            var pokemon = _mapper.Map<Pokemon>(_pokemonRepoitory.GetPokemon(pokeId));
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(pokemon);
+        }
+
+        [HttpGet("{pokeId}/rating")]
+        [ProducesResponseType(200, Type = typeof(decimal))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemonRating(int pokeId)
+        {
+            if (!_pokemonRepoitory.PokemonExists(pokeId))
+               return NotFound();
+
+               var rating = _pokemonRepoitory.GetPokemonRating(pokeId);
+
+               if (!ModelState.IsValid)
+                   return BadRequest();
+
+               return Ok(rating);
+           
         }
     }
 }
