41af326c784b288d632469f774d2795a0956ffd5
Trexxin <Charlesharris2322@gmail.com>
2022-06-29 09:22:40 -0500

    Added Requests to Controller

--- PokemonReviewApp/Controllers/CategoryController.cs
+++ PokemonReviewApp/Controllers/CategoryController.cs
@@ -28,7 +28,37 @@ namespace PokemonReviewApp.Controllers
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
 
+            return Ok(categories);
+        }
+
+        [HttpGet("{categoryId}")]
+        [ProducesResponseType(200, Type = typeof(Category))]
+        [ProducesResponseType(400)]
+        public IActionResult GetCategory(int categoryId)
+        {
+            if (!_categoryRepository.CategoryExists(categoryId))
+                return NotFound();
+
+            var category = _mapper.Map<CategoryDto>(_categoryRepository.GetCategory(categoryId));
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(category);
+        }
+
+        [HttpGet("pokemon/{categoryId}")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemonByCategoryId(int categoryId)
+        {
+            var pokemons = _mapper.Map<List<PokemonDto>>(_categoryRepository.GetPokemonByCategory(categoryId));
+
+            if (!ModelState.IsValid)
+                return BadRequest();
+
             return Ok(pokemons);
         }
     }
+
 }
