d324c265d66997c5680ae67e720a5b7c40970940
Trexxin <Charlesharris2322@gmail.com>
2022-06-30 12:37:11 -0500

    Added ReviewerDto

---
+++ PokemonReviewApp/Dto/ReviewerDto.cs
@@ -0,0 +1,9 @@
+﻿namespace PokemonReviewApp.Dto
+{
+    public class ReviewerDto
+    {
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
