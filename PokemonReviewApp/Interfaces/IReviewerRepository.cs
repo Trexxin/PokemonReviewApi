3fe397dd36bdc9ad003af3653276f093d6a9d4c0
Trexxin <Charlesharris2322@gmail.com>
2022-06-30 12:12:51 -0500

    Added ReviewerRepository Interface

---
+++ PokemonReviewApp/Interfaces/IReviewerRepository.cs
@@ -0,0 +1,12 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interfaces
+{
+    public interface IReviewerRepository
+    {
+        ICollection<Reviewer> GetReviewers();
+        Reviewer GetReviewer(int reviewId);
+        ICollection<Review> GetReviewsByReviewer(int reviewId);
+        bool ReviewerExists();
+    }
+}
