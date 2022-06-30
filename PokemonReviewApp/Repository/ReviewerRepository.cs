b625bd0c2b236918e36878c1bbf312e04816c395
Trexxin <Charlesharris2322@gmail.com>
2022-06-30 12:29:35 -0500

    Added ReviewerRepository

---
+++ PokemonReviewApp/Repository/ReviewerRepository.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class ReviewerRepository : IReviewerRepository
+    {
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+
+        public ReviewerRepository(DataContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public Reviewer GetReviewer(int reviewId)
+        {
+            return _context.Reviewers.Where(r => r.id == reviewId).Include(e => e.Reviews).FirstOrDefault();
+        }
+
+        public ICollection<Reviewer> GetReviewers()
+        {
+            return _context.Reviewers.ToList();
+        }
+
+        public ICollection<Review> GetReviewsByReviewer(int reviewId)
+        {
+            return _context.Reviews.Where(r => r.Reviewer.id == reviewId).ToList();
+        }
+
+        public bool ReviewerExists(int reviewerId)
+        {
+            return _context.Reviewers.Any(r => r.id == reviewerId);
+        }
+    }
+}
