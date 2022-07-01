1fc47006eb1fde66e86a293ac2100fb7088474c2
Trexxin <Charlesharris2322@gmail.com>
2022-07-01 18:28:10 -0500

    Added Update Method/Request for Pokemon

--- PokemonReviewApp/Controllers/PokemonController.cs
+++ PokemonReviewApp/Controllers/PokemonController.cs
@@ -12,11 +12,13 @@ namespace PokemonReviewApp.Controllers
     {
         private readonly IPokemonRepository _pokemonRepository;
         private readonly IMapper _mapper;
+        private readonly IReviewerRepository _reviewerRepository;
 
-        public PokemonController(IPokemonRepository pokemonRepository, IMapper mapper)
+        public PokemonController(IPokemonRepository pokemonRepository, IMapper mapper, IReviewerRepository reviewerRepository)
         {
             _pokemonRepository = pokemonRepository;
             _mapper = mapper;
+            _reviewerRepository = reviewerRepository;
         }
 
         [HttpGet]
@@ -92,6 +94,34 @@ namespace PokemonReviewApp.Controllers
             }
 
             return Ok("Sucessfully Created");
+        }
+
+        [HttpPut("{pokeId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public IActionResult UpdateCategory(int pokeId, [FromQuery] int ownerId, [FromQuery] int catId, [FromBody] PokemonDto updatedPokemon)
+        {
+            if (updatedPokemon == null)
+                return BadRequest(ModelState);
+
+            if (pokeId != updatedPokemon.Id)
+                return BadRequest(ModelState);
+
+            if (!_pokemonRepository.PokemonExists(pokeId))
+                return NotFound();
+
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var pokemonMap = _mapper.Map<Pokemon>(updatedPokemon);
+
+            if (!_pokemonRepository.UpdatePokemon(ownerId, catId, pokemonMap))
+            {
+                ModelState.AddModelError("", "Something went wrong updating Pokemon");
+                return StatusCode(500, ModelState);
+            }
+            return NoContent();
         }
     }
 }
