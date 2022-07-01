49e3ea88e279e7aae00ee8aa5e6c7ed9ef278000
Trexxin <Charlesharris2322@gmail.com>
2022-07-01 14:15:58 -0500

    Added Update Method/Request

--- PokemonReviewApp/Controllers/CountryController.cs
+++ PokemonReviewApp/Controllers/CountryController.cs
@@ -89,5 +89,33 @@ namespace PokemonReviewApp.Controllers
 
             return Ok("Sucessfully Created");
         }
+
+        [HttpPut("{countryId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public IActionResult UpdateCountry(int countryId, [FromBody] CountryDto updatedCountry)
+        {
+            if (updatedCountry == null)
+                return BadRequest(ModelState);
+
+            if (countryId != updatedCountry.Id)
+                return BadRequest(ModelState);
+
+            if (!_countryRepository.CountryExists(countryId))
+                return NotFound();
+
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var countryMap = _mapper.Map<Country>(updatedCountry);
+
+            if (!_countryRepository.UpdateCountry(countryMap))
+            {
+                ModelState.AddModelError("", "Something went wrong updating the country");
+                return StatusCode(500, ModelState);
+            }
+            return NoContent();
+        }
     }
 }
