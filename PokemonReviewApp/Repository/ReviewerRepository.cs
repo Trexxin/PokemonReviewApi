89953defeedc238773e9ed0ca0e3aa0bc8289677
Trexxin <Charlesharris2322@gmail.com>
2022-07-01 11:09:52 -0500

    Added Create and Save methods

--- PokemonReviewApp/Repository/ReviewerRepository.cs
+++ PokemonReviewApp/Repository/ReviewerRepository.cs
@@ -17,6 +17,12 @@ namespace PokemonReviewApp.Repository
             _mapper = mapper;
         }
 
+        public bool CreateReviewer(Reviewer reviewer)
+        {
+            _context.Add(reviewer);
+            return Save();
+        }
+
         public Reviewer GetReviewer(int reviewId)
         {
             return _context.Reviewers.Where(r => r.id == reviewId).Include(e => e.Reviews).FirstOrDefault();
@@ -35,6 +41,12 @@ namespace PokemonReviewApp.Repository
         public bool ReviewerExists(int reviewerId)
         {
             return _context.Reviewers.Any(r => r.id == reviewerId);
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
         }
     }
 }
