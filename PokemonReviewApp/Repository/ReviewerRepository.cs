84ea64997702e3b885effe422f1a819a17192590
Trexxin <Charlesharris2322@gmail.com>
2022-07-02 11:28:35 -0500

    Added Delete Method/Request for Reviewer

--- PokemonReviewApp/Repository/ReviewerRepository.cs
+++ PokemonReviewApp/Repository/ReviewerRepository.cs
@@ -23,6 +23,12 @@ namespace PokemonReviewApp.Repository
             return Save();
         }
 
+        public bool DeleteReviewer(Reviewer reviewer)
+        {
+            _context.Remove(reviewer);
+            return Save();
+        }
+
         public Reviewer GetReviewer(int reviewId)
         {
             return _context.Reviewers.Where(r => r.id == reviewId).Include(e => e.Reviews).FirstOrDefault();
