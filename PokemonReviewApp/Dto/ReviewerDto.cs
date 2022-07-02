75d0920c25d1ea3284f0258a4272d8218f6c992d
Trexxin <Charlesharris2322@gmail.com>
2022-07-02 07:35:45 -0500

    Added Update Method/Request for Reviewer

--- PokemonReviewApp/Dto/ReviewerDto.cs
+++ PokemonReviewApp/Dto/ReviewerDto.cs
@@ -4,7 +4,7 @@ namespace PokemonReviewApp.Dto
 {
     public class ReviewerDto
     {
-        public int id { get; set; }
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
     }
